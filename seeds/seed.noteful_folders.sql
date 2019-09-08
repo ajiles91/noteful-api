a9faa5720fc629efa2093aad81e1f04b773c64b6
Amber <ajiles91@gmail.com>
2019-09-07 22:23:13 -0400

    first commit

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,6 @@
+INSERT INTO noteful_folders ("name")
+VALUES
+  ('Folder One'),
+  ('Folder Two'),
+  ('Folder Three')
+;
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,6 @@
+INSERT INTO notes ("name", content, folder_id)
+VALUES
+  ('Note One', 'A note', 1),
+  ('Note Two', 'A second note', 2),
+  ('Note Three', 'A third note', 3)
+;
